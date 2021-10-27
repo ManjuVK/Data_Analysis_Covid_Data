e2118631f972402cc83729eb8563ec4d337616a5
ManjuVK <manjuvkurup16@gmail.com>
2021-10-27 20:38:29 +0100

    sample code for just testing

--- sample_code_MM.py
+++ sample_code_MM.py
@@ -73,8 +73,8 @@ x1 = np.arange(1, 567)
 log_Uk_dataset = np.log(normalised_United_Kingdom_dataset)
 log_Uk_dataset = log_Uk_dataset.dropna()
 y1 = log_Uk_dataset
-UK_first_wave = y1[1:200]
-UK_second_wave = y1[200:567]
+UK_first_wave = y1[1:201]
+UK_second_wave = y1[201:]
 plt.xticks(np.arange(1,567,50))
 plt.xlim(xmin=0)
 plt.xticks(rotation=0)
@@ -95,9 +95,9 @@ plt.plot(x1, y1, '--g')
 plt.grid()
 plt.show()
 # first wave
-x1 = np.arange(1,200)
+x1 = np.arange(1,201)
 y1 = UK_first_wave
-plt.xticks(np.arange(1,200,50))
+plt.xticks(np.arange(1,201,50))
 plt.ylabel("Number of Days")
 plt.xlabel("Cumulative cases")
 plt.title("First wave of UK")
@@ -108,7 +108,44 @@ x1, y1 = [37, 199], [-6.67, -5.11]
 plt.plot(x1, y1, '--r')
 plt.grid()
 plt.show()
-
+#finding slope and intercept
+x1 = np.arange(1,38)
+UK_firstsegment = UK_first_wave[1:38]
+y1 = UK_firstsegment
+UK_slope_intercept1 = np.polyfit(x1, y1, 1)
+print("UK First wave- slope & intercept")
+print(UK_slope_intercept1)
+slope = 0.15483199
+intercept = -11.70299942
+UK_exp_model1 = np.exp(intercept + slope * x1)
+y1 = UK_exp_model1
+y2 = normalised_United_Kingdom_dataset[1:38]
+y3 = y2 - y1
+plt.title("Exponential growth of UK for first segment in Wave 1 ")
+plt.xlabel("Days")
+plt.ylabel("Number of cases")
+plt.plot(x1, y1 ,label="Predicted wave",c="r")
+plt.plot(x1, y2 ,label="Observed wave",c="b")
+plt.plot(x1, y3, label = "Error", c="c")
+plt.grid()
+plt.legend()
+plt.show()
+#prediction for 1st UK wave
+x1 = np.arange(1,201)
+y1 = normalised_United_Kingdom_dataset[1:201]
+UK_exp_model_firstwave = np.exp(intercept + slope * x1)
+y1 = UK_exp_model_firstwave
+y2 = normalised_United_Kingdom_dataset[1:201]
+y3 = y2 - y1
+plt.title("Exponential growth of UK for first Wave ")
+plt.xlabel("Days")
+plt.ylabel("Number of cases")
+plt.plot(x1, y1 ,label="Predicted wave",c="r")
+plt.plot(x1, y2 ,label="Observed wave",c="b")
+plt.plot(x1, y3, label = "Error", c="c")
+plt.grid()
+plt.legend()
+plt.show()
 #second wave
-UK_second_wave = y1[200:]
+
 
